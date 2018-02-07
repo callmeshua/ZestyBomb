d2bc002627fb4f04e2e115591605ef259d29cf3c
Zachary Lopez <ztl25@drexel.edu>
2018-02-06 22:47:33 -0500

    Modified catapult WIP

--- Flatlander/Assets/Scripts/Catapult.cs
+++ Flatlander/Assets/Scripts/Catapult.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class Catapult : MonoBehaviour
 {
-
+    private Rigidbody rb;
+    private float z;
 	// Use this for initialization
 	void Start ()
 	{
-	
-	}
+        rb = gameObject.GetComponent<Rigidbody>();
+        z = gameObject.transform.eulerAngles.x;
+        
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
-	}
+        gameObject.transform.Rotate(0, 0, 15 * Time.deltaTime);
+        if (gameObject.transform.eulerAngles.x >= 90)
+        {
+            z = 90f;
+        }
+    }
 }
 
