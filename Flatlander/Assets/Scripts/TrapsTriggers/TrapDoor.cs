4adfa08faeb95fb1909b9b7ba6dcb78be274eb38
Hugo Armella <hugosarmellaf@gmail.com>
2018-02-07 01:05:15 -0500

    Added TrapDoor prefab.
    Trap Door prefab added. Currently has no model, but the functionality is there! Works just like other traps in the sense that it can be activated by a Trigger, but it can also activate if the player collides with it.

---
+++ Flatlander/Assets/Scripts/TrapsTriggers/TrapDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDoor : Trap {
+
+    Rigidbody doorRb;
+
+	// Use this for initialization
+	void Start () {
+        active = false;
+        doorRb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+    public void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.gameObject.tag == "Player")
+            active = true;
+    }
+
+    public override void checkActive()
+    {
+        if(active)
+        {
+            doorRb.isKinematic = false;
+        }
+    }
+}
