2bddbba6ffca42e46dd6507b421e383418f9ca99
Jack Phoebus <jmp1996.jp@gmail.com>
2018-02-07 15:46:03 -0500

    order has been restored
    looks good fam

---
+++ Flatlander/Assets/Scripts/WinArea.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * JOSH KARMEL
+ * USED
+ * 
+ * ATTACHED TO THE WIN AREA OBJECT
+*/
+
+public class WinArea : MonoBehaviour {
+
+    //PUBLIC REFERENCED SCRIPTS
+    public ParameterScreen ps;      //to pause the game
+    public GM gm;
+    public AudioClip winSound;
+
+    public bool win;
+
+	// Use this for initialization
+	void Start () {
+        ps = FindObjectOfType<ParameterScreen>();
+        gm = FindObjectOfType<GM>();
+        win = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Tab))
+        {
+            //transitions from exploration to escape at relic area
+            if (tag == "relicArea" && gm.phase == GM.Phases.EXPLORE)
+            {
+                SoundManager.PlaySFX(winSound, false, .6f);
+                gm.triggerEscape();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    //transitions between phases
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //transitions from exploration to escape at relic area
+            if (tag == "relicArea" && gm.phase == GM.Phases.EXPLORE)
+            {
+                SoundManager.PlaySFX(winSound, false, .6f);
+                gm.triggerEscape();
+                Destroy(gameObject);
+            }
+
+            //win condition for escape area
+            if (tag == "exitArea" && gm.phase == GM.Phases.ESCAPE)
+            {
+                SoundManager.PlaySFX(winSound, false, .6f);
+                win = true;
+                gm.paused = true;
+            }
+        }
+    }
+}
