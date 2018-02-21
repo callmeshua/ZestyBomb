107d70272af40011663f8085652e4d9ef0589d51
Ian Elrath <ire23@drexel.edu>
2018-02-21 16:26:30 -0500

    Swictches

---
+++ Flatlander/Assets/Scripts/TrapsTriggers/Triggers/Switch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : Trigger {
+
+    protected Vector3 pos;
+    protected Vector3 playerPos;
+
+    protected KeyCode key;
+
+    public GameObject model;
+    public bool isActive;
+    public float unit;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        pos = model.transform.position;
+
+        key = KeyCode.E;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(key))
+        {
+            playerPos = player.transform.position;
+            if (Vector3.Distance(playerPos, pos) < unit)
+            {
+                
+                if (!isActive)
+                { 
+                    isActive = !isActive;
+                    trigger();
+                }
+                else
+                {
+                    isActive = !isActive;
+                    detrigger();
+                }
+            }
+        }      
+	}
+}
