4b69a6b5cba53cd0bea20456e27cd09f828e979e
Zachary Lopez <ztl25@drexel.edu>
2018-02-25 14:37:55 -0500

    Scaling object WIP

--- Flatlander/Assets/Scripts/EnvironmentScripts/ScaleObject.cs
+++ Flatlander/Assets/Scripts/EnvironmentScripts/ScaleObject.cs
@@ -6,13 +6,20 @@ public class ScaleObject : MonoBehaviour {
 	private float x;
 	private float y;
 	private float z;
+
 	// Use this for initialization
 	void Start () {
-		
+		x = transform.position.x;
+		y = transform.position.y;
+		z = transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		x = transform.position.x;
+		y = transform.position.y;
+		z = transform.position.z;
+
+		TextureScale.Bilinear()
 	}
 }
