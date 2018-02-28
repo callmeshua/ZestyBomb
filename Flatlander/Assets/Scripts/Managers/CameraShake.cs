fccaa371a1ea9de4bae725d0e36683d1437bb216
callmeshua <callmeshua620@gmail.com>
2018-02-27 23:04:08 -0500

    scripts back in

---
+++ Flatlander/Assets/Scripts/Managers/CameraShake.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    // Set to the CamRig
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shakeDuration;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount;
+    public float decreaseFactor;
+
+    Vector3 originalPos;
+
+    private void Start()
+    {
+        shakeDuration = 0f;
+        shakeAmount = 0.7f;
+        decreaseFactor = 1f;
+    }
+
+    void Awake()
+    {
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+            
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
+    }
+
+    //sets duration of camera shake
+    public void setDuration(float dur)
+    {
+        shakeDuration = dur;
+    }
+}
