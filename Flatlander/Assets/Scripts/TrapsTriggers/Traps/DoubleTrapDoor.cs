da9a0bd18f9e39fd2c50eaa579bdafdc81cf9526
Hugo Armella <hugosarmellaf@gmail.com>
2018-02-28 17:19:13 -0500

    Added DoubleTrapDoor script & Prefab

---
+++ Flatlander/Assets/Scripts/TrapsTriggers/Traps/DoubleTrapDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleTrapDoor : Trap {
+
+    private GameObject trapDoor1;
+    private GameObject trapDoor2;
+    
+	// Use this for initialization
+	void Start () {
+        active = false;
+        trapDoor1 = transform.GetChild(0).gameObject;
+        trapDoor2 = transform.GetChild(1).gameObject;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            active = true;
+        }
+    }
+
+    public override void checkActive()
+    {
+        if (active)
+        {
+            trapDoor1.GetComponent<TrapDoor>().activate();
+            trapDoor2.GetComponent<TrapDoor>().activate();
+        }
+    }
+}
