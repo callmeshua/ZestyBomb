a1c064cdab86f59ffb9a8d7de0be1d01a0b94fa7
Zachary Lopez <ztl25@drexel.edu>
2018-03-04 17:45:48 -0500

    Lever WIP

--- Flatlander/Assets/Scripts/TrapsTriggers/Triggers/Lever.cs
+++ Flatlander/Assets/Scripts/TrapsTriggers/Triggers/Lever.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Lever : MonoBehaviour {
     private Transform trans;
+    private float z;
 	// Use this for initialization
 	void Start () {
-	
+        trans = transform;
+        z = trans.eulerAngles.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        trans.Rotate(new Vector3(0, 0, z * Time.deltaTime));
 	}
 }
