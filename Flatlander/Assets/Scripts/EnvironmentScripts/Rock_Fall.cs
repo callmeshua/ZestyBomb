d541348b8c98e23e68b5a4541db624fd157f6de0
3Ply <33212849+3Ply@users.noreply.github.com>
2018-03-14 17:40:34 -0400

    lvl 2 updates+camcontroller changes

--- Flatlander/Assets/Scripts/EnvironmentScripts/Rock_Fall.cs
+++ Flatlander/Assets/Scripts/EnvironmentScripts/Rock_Fall.cs
@@ -65,6 +65,10 @@ public class Rock_Fall : MonoBehaviour {
 		} else if (col.gameObject.tag == "Hazard") {
 			Physics.IgnoreCollision (gameObject.GetComponent<Collider> (), col.gameObject.GetComponent<Collider> (), true);
 		} 
+        else if(col.gameObject.tag=="Lava")
+        {
+            Destroy(gameObject);
+        }
         else
         {
             SoundManager.PlaySFX(impactClip, true, Mathf.Clamp01(col.impulse.magnitude/80f));
